ae6805e0eef3e0309506beff05146ff5823e1f0a
Jeihun Lee <kismetro@gmail.com>
2022-03-16 21:03:14 +0900

    [FIX] with flow

--- util/InputCtrl.py
+++ util/InputCtrl.py
@@ -98,6 +98,7 @@ class intpuCtrl:
     def showDb(self):
         mp_drawing = mp.solutions.drawing_utils
         mp_hands = mp.solutions.hands
+     
         
         with mp_hands.Hands(
             max_num_hands=1,
@@ -111,20 +112,20 @@ class intpuCtrl:
     
             results = hands.process(image)
     
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    finger1 = int(hand_landmarks.landmark[4].x * 100 )
-                    finger2 = int(hand_landmarks.landmark[8].x * 100 )
-                    dist = abs(finger1 - finger2)
-                    cv2.putText(
-                        image, text='f1=%d f2=%d dist=%d ' % (finger1,finger2,dist), org=(10, 30),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                        color=255, thickness=3)
-    
-                    mp_drawing.draw_landmarks(
-                        image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                finger1 = int(hand_landmarks.landmark[4].x * 100 )
+                finger2 = int(hand_landmarks.landmark[8].x * 100 )
+                dist = abs(finger1 - finger2)
+                cv2.putText(
+                    image, text='f1=%d f2=%d dist=%d ' % (finger1,finger2,dist), org=(10, 30),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                    color=255, thickness=3)
+
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         return image, ret
 
